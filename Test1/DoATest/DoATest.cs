3db0b807e3369acd9828e655c964a806ffde7a01
Asho <angryasho@gmail.com>
2017-11-23 10:13:52 +0200

    Added a test project

---
+++ Test1/DoATest/DoATest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoATest
+{
+    class DoATest
+    {
+        static void Main()
+        {
+            Console.WriteLine("If this line is here, then everythinghas gone successfull! :)");
+        }
+    }
+}
